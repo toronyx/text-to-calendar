3b5ba758c66ae4e7b9d030b871330961b76647bf
toronyx <tommi.ronayne@taurho.co.uk>
2026-04-14 02:51:02 +0100

    flowed through error messages straight from gemini - bad idea maybe? idk

--- server/ttc_server/exception_handlers.py
+++ server/ttc_server/exception_handlers.py
@@ -7,20 +7,14 @@ from ttc_server.clients.llm.llm_client import LLMException
 async def llm_exception_handler(request: Request, exc: LLMException):
     cause = exc.__cause__
     if isinstance(cause, genai.errors.ClientError):  # add more error types here for other LLMs as needed
-        detail = {
-            429: "Gemini API request limit reached!",
-        }.get(cause.code, f"Gemini Client error {cause}")
         return JSONResponse(
             status_code=422,  # unprocessable
-            content={"detail": detail},
+            content={"detail": cause.message},
         )
     elif isinstance(cause, genai.errors.ServerError):  # add more error types here for other LLMs as needed
-        detail = {
-            503: "Gemini server is currently overloaded! Try again later.",
-        }.get(cause.code, f"Gemini Server error {cause}")
         return JSONResponse(
             status_code=502,  # upstream service failed
-            content={"detail": detail},
+            content={"detail": cause.message},
         )
     return JSONResponse(
         status_code=502,  # upstream service failed
